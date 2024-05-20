2b5b66d390dd8f6fb70581f4046989ce9c383f99
turnip99 <turnipz99@gmail.com>
2024-05-20 19:04:13 +0100

    Media files attempt

--- jyutpinglearningsite/urls.py
+++ jyutpinglearningsite/urls.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from django.urls import include, path
+from django.views.static import serve
+from django.urls import include, path, re_path
 
 from . import views
 from .decorators import logged_out_required
@@ -13,6 +14,5 @@ urlpatterns = [
     path('login/', logged_out_required(views.LoginView.as_view()), name='login'),
     path('logout/', login_required(views.LogoutView.as_view(), login_url="/login/"), name='logout'),
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
