a1873f9828bacf41f171a10d0f351cdc1ec2fe5f
turnip99 <turnipz99@gmail.com>
2024-05-26 20:18:17 +0100

    Remove import export app

--- jyutpinglearningsite/settings.py
+++ jyutpinglearningsite/settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'import_export',
     'crispy_forms',
     'crispy_bootstrap5'
 ]
